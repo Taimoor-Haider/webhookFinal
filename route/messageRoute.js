dc293215d3e89d2c3892060f78099f352c3dec41
Taimoor Haider <116557295+Taimoor-Haider@users.noreply.github.com>
2024-08-08 15:23:11 +0500

    Update messageRoute.js

--- route/messageRoute.js
+++ route/messageRoute.js
@@ -4,12 +4,13 @@ import {
   searchMessagesByAuthorName,
   getAllWebhooks,
   testWebhook,
+  addMessageWebhook
 } from "../controller/messageController.js";
 const router = express.Router();
 
 // Route to search for messages
 router.get("/search/messages", searchMessagesByAuthorName);
-
+router.post("/send-message",addMessageWebhook);
 // Route to get all webhooks
 router.get("/messages", getAllWebhooks);
 // Route to get all webhooks By Message
